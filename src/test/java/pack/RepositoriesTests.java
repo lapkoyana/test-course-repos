4a2875be47c283195cf04643d90eb1575f688f60
lapkoyana <lapko.yan@gmail.com>
2021-07-20 17:21:14 +0400

    Fixed a bug in the getRepositoryTest method

--- src/test/java/pack/RepositoriesTests.java
+++ src/test/java/pack/RepositoriesTests.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -18,27 +19,23 @@ public class RepositoriesTests {
 	RepositoriesSteps ts = new RepositoriesSteps();
 	static List<GitRepository> actualRepositories;
 
+	@Test
 	@BeforeAll
 	public void getRepositoryListTest() {
 		actualRepositories = ts.getAllReps("selenide");
-		System.out.println(actualRepositories);
 
 		Assertions.assertTrue(actualRepositories.size() > 0, "the repository list is empty");
-		System.out.println("после getRepositoryListTest");
 	}
 
 	@ParameterizedTest
 	@MethodSource("getAllRepsNamesAsList")
 	public void getRepositoryTest(String repoName) {
-		System.out.println("до getRepositoryTest");
-
 		GitRepository currentRepository = ts.getRepository("selenide", repoName);
 
 		String fullName = currentRepository.getFullName();
 
 		Assertions.assertTrue(currentRepository.getFullName() != null, "repository is not found");
-		Assertions.assertEquals(fullName, "selenide/selenide-ru", "fullName does not match the intended");
-		System.out.println("после getRepositoryTest");
+		Assertions.assertEquals(fullName, "selenide/" + repoName, "fullName does not match the intended");
 	}
 
 	private static List<String> getAllRepsNamesAsList() {
